418d664f303be7480c70ccfc00f00219de73c242
Solomon Sitotaw <50806926+SolomonSitotaw23@users.noreply.github.com>
2021-10-29 14:39:09 +0300

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
-
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
+  const [BurgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a href="#">
@@ -20,8 +21,37 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
+      <BurgerNav show={BurgerStatus}>
+        <CloseWrap>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrap>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-In</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">SemiCharging</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -37,6 +67,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 
 const Menu = styled.div`
@@ -67,3 +98,29 @@ const RightMenu = styled.div`
   cursor: pointer;
 `;
 const CustomMenu = styled(MenuIcon)``;
+const BurgerNav = styled.div`
+  list-style-type: none;
+  text-align: left;
+  width: 250px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 10;
+  padding: 20px;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.2s ease-in-out;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
+const CloseWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+const CustomClose = styled(CloseIcon)``;
